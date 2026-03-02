ef2adc2261d022bf22387843fc6788b75e7ac996
Mennatullah-Magdy-Abdalwahab <menna83652@gmail.com>
2026-03-03 01:56:02 +0200

    Update average.java

--- coding/average.java
+++ coding/average.java
@@ -1,3 +1,18 @@
 //I will send you an array of integers that contain 5 numbers 
 //return the average of them (as integer if its double round to nearest integer)
-//Example: [1,2,3,4,5] -> 3
+//Example: [1,2,3,4,5] -> 3package com.mycompany.averageminiproject;
+
+public class Average {
+
+    public static double calculate(double[]grade){
+
+        double sum = 0;
+
+        for (int i=0;i<5;i++) {
+            sum+= grade[i];
+        }
+
+        return sum/5;
+    }
+
+}
